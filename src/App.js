172fda5a8652b33cfee7081618f0705f86ff238c
PabloMD <chmielecki7@gmail.com>
2021-03-20 14:14:41 +0100

    show all components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import ExpenseCalculator from './components/ExpenseCalculator/ExpenseCalculator';
 
-// import FormikValidation from './components/FormikValidation';
-// import FormValidation from './components/FormValidation';
-// import RestaurantCalc, { RestaurantCalcFunc } from './components/RestaurantCalc';
+import FormikValidation from './components/FormikValidation';
+import FormValidation from './components/FormValidation';
+import RestaurantCalc, { RestaurantCalcFunc } from './components/RestaurantCalc';
 
 
 function App() {
   return (
     <div className="App">
       <ExpenseCalculator />
-      {/* <FormikValidation /> */}
-      {/* <FormValidation /> */}
-      {/* <RestaurantCalc /> */}
-      {/* <RestaurantCalcFunc /> */}
+      <FormikValidation />
+      <FormValidation />
+      <RestaurantCalc />
+      <RestaurantCalcFunc />
     </div>
   );
 }
